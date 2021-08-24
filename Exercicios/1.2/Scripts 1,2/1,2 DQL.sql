95d6e5d7915a0781776d4c6e337c21c33a731d8d
Matheus O.G. do Nascimento <82417324+Matheus-O-G-do-Nascimento@users.noreply.github.com>
2021-08-24 01:05:22 -0300

    Add files via upload

---
+++ Exercicios/1.2/Scripts 1,2/1,2 DQL.sql
@@ -0,0 +1,16 @@
+USE Locadora;
+GO
+
+SELECT * FROM Empresa
+ORDER BY idEmpresa
+
+SELECT * FROM Cliente
+
+SELECT * FROM Marca
+ORDER BY idMarca
+
+SELECT * FROM Modelo
+
+SELECT * FROM Veículo
+
+SELECT * FROM Aluguel
